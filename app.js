ef53808ad548f9e7134540832bb11b69773e63aa
AnaBeCosta <anacost.a@outlook.com>
2023-05-06 13:59:02 -0300

    Alterando conexão com banco de dados

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-const db_mongoose = require('./config/db_mongoose');
-const mongoose = require('mongoose');
 const routes = require('./routers/route');
 const express = require('express');
+const connectToDatabase = require('./config/db_mongoose');
 const app = express();
 
+connectToDatabase();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-
-mongoose.connect(db_mongoose.connection, {useUnifiedTopology:true, useNewUrlParser:true}).then(()=>{
-    console.log('Conectado com o BD do mongo');
-}).catch(()=>{
-        console.log('Erro na conexão com o BD');
-});
 
 app.listen(8081, function(){
 console.log("Servidor no http://localhost:8081")
--- config/db_mongoose.js
+++ config/db_mongoose.js
@@ -1,5 +1,19 @@
-const StringCon = {
-    connection: "mongodb+srv://anabeatrizcosta:anajulia@administrador.tvg3nut.mongodb.net/test"
+const mongoose = require('mongoose');
+
+
+function connectToDatabase(){
+    mongoose.connect(
+        "mongodb+srv://adm:UTFPR123@projetos.yu7r5mm.mongodb.net/WEB2?retryWrites=true&w=majority", 
+            {
+                useUnifiedTopology:true, 
+                useNewUrlParser:true
+            }
+    );
+
+    const db = mongoose.connection;
+    db.on("error", (error) => console.error(error));
+    db.once("open", () => console.log("Connected to the database!"));
 }
 
-module.exports = StringCon;
+
+module.exports = connectToDatabase;
