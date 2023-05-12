47436ca1aefb149814288d8bf2c1a6f3312ca24f
AnaBeCosta <anacost.a@outlook.com>
2023-05-12 14:47:42 -0300

    Adicionando bootstrap, tela login.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.engine('handlebars', handlebars.engine({defaultLayout:'noMenu'}));
 app.set('view engine','handlebars');
 
-
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({secret: 'textosecreto', saveUninitialized: true, cookie: {maxAge: 30*60*1000}}));
